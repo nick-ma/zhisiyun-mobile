5fb54d7c2da30234daaae1a966bb19da0848db92
Nick Ma <honeyday.mj@gmail.com>
2014-04-16 18:32:51 +0800

    make jqm calendar works

--- views/TaskView.js
+++ views/TaskView.js
@@ -2,7 +2,7 @@
 // =============
 
 // Includes file dependencies
-define(["jquery", "underscore", "backbone", "handlebars", "models/TaskModel","jqmcal"], function($, _, Backbone, Handlebars, TaskModel) {
+define(["jquery", "underscore", "backbone", "handlebars", "models/TaskModel", "jqmcal"], function($, _, Backbone, Handlebars, TaskModel) {
 
 
     // var Handlebars = HB.
@@ -17,7 +17,21 @@ define(["jquery", "underscore", "backbone", "handlebars", "models/TaskModel","jq
 
             // The render method is called when Task Models are added to the Collection
             this.collection.on("sync", this.render, this);
-
+            $("#jqm_cal").jqmCalendar({
+                events: [{
+                    "summary": "第一个",
+                    "begin": new Date('2014-04-01'),
+                    "end": new Date('2014-04-08')
+                }, {
+                    "summary": "Test event",
+                    "begin": new Date(),
+                    "end": new Date()
+                }],
+                months: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
+                days: ["日", "一", "二", "三", "四", "五", "六"],
+                startOfWeek: 0,
+                route: 'task' //pass current page route to widget, to avoid page change when click date.
+            });
         },
 
         // Renders all of the Task models on the UI
@@ -26,33 +40,16 @@ define(["jquery", "underscore", "backbone", "handlebars", "models/TaskModel","jq
             // Sets the view's template property
             // console.log(self.collection);
             var rendered = [];
-            rendered = _.map(this.collection.models, function(x) {
-                // console.log(x);
-                return self.template(x.attributes);
-            })
+            // rendered = _.map(this.collection.models, function(x) {
+            //     // console.log(x);
+            //     return self.template(x.attributes);
+            // })
             // console.log(rendered);
             // Renders the view's template inside of the current listview element
             self.$el.find("ul").html(rendered.join(''));
             //jqmCalendar test
 
-            $("#jqm_cal").jqmCalendar({
-                events: [{
-                    "summary": "Test event",
-                    "begin": new Date("2013-02-05 00:00:00"),
-                    "end": new Date("2013-02-07 00:00:00")
-                }, {
-                    "summary": "Test event",
-                    "begin": new Date(),
-                    "end": new Date()
-                }, {
-                    "summary": "Test event",
-                    "begin": new Date(),
-                    "end": new Date()
-                }],
-                months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-                days: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
-                startOfWeek: 0
-            });
+
             // Maintains chainability
             return this;
 
