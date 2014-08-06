d6d6be789e8ff5044be4682d77e9e2f4947e2bf0
GISI Dev 01 <gisidev01@ubuntu.(none)>
2014-08-06 17:51:44 +0800

    NBTI

--- views/quesetionnaire/EditGradeMBTIList.js
+++ views/quesetionnaire/EditGradeMBTIList.js
@@ -29,7 +29,6 @@ define(["jquery", "underscore", "backbone", "handlebars", "async"], function($, 
         // Renders all of the Task models on the UI
         render: function() {
             var self = this;
-            console.log(self);
             var rendered_data = '';
             $("#nbti_name").html('NBTI测试')
             if (self.model_view == '0') {
