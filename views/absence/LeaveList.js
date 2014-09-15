c7614b4d5efcfc74ca8fa7f113d0f4a58f19f40f
blithe <blithe@blithedeMacBook-Pro.local>
2014-09-15 16:34:16 +0800

    blithe

--- views/absence/LeaveList.js
+++ views/absence/LeaveList.js
@@ -23,6 +23,15 @@ define(["jquery", "underscore", "backbone", "handlebars", "async", "moment"], fu
         return str
     });
 
+
+    Handlebars.registerHelper('change_type', function(data) {
+        return validate(data) ? '收 入' : '支 出';
+    });
+
+    Handlebars.registerHelper('dis_type', function(data) {
+        return validate(data) ? '请 假' : '消 假';
+    });
+
     Handlebars.registerHelper('show_date', function(st, ed) {
         var s = (st) ? moment(st).format('YYYY-MM-DD') : '';
         var e = (ed) ? moment(ed).format('YYYY-MM-DD') : '';
@@ -39,23 +48,123 @@ define(["jquery", "underscore", "backbone", "handlebars", "async", "moment"], fu
         return num;
     });
 
+    function validate(num) {
+        var reg = /^\d+(?=\.{0,1}\d+$|$)/
+        if (reg.test(num)) return true;
+        return false;
+    }
+
+    function calculate(items) {
+        var num = 0;
+        _.each(items, function(it) {
+            num += it.balance
+        })
+        return num
+    }
+
+    function sort(items) {
+        var sorts = _.sortBy(items, function(it) {
+            return it.start_date
+        })
+        return sorts.reverse()
+    }
 
     var AbsenceListView = Backbone.View.extend({
         // The View Constructor
         initialize: function() {
-            console.log(this);
             this.leave_template = Handlebars.compile($("#leave_view").html());
+            this.balance_template = Handlebars.compile($("#balance_view").html());
+            this.balance_total_template = Handlebars.compile($("#balance_total_view").html());
             this.month = moment(new Date()).month() + 1;
             this.bind_event();
+            this.mode_view = '0';
         },
         // Renders all of the Task models on the UI
         render: function() {
             var self = this;
             var rendered_data = '';
-            var items = self.collection.toJSON()
-            rendered_data = self.leave_template({
-                leaves: items.reverse()
-            });
+            var items = self.collection.toJSON();
+            _.each($("#leave_list-left-panel label"), function(x) {
+                var state = $(x).data('state');
+                if (state == '0') {
+                    $(x).find('span').html(items[0].leaves.length || 0);
+                } else if (state == '1') {
+                    $(x).find('span').html(items[0].balance.details.length || 0);
+                } else if (state == '2') {
+                    var all_cunts = _.flatten([items[0].leave_total.leaveOfabsences, items[0].leave_total.backleaveOfabsences], true)
+                    $(x).find('span').html(all_cunts.length || 0);
+                };
+            })
+
+            if (self.mode_view == '0') {
+                rendered_data = self.leave_template({
+                    leaves: items[0].leaves.reverse()
+                });
+            } else if (self.mode_view == '1') {
+                var current_date = moment(new Date()).format('YYYY-MM-DD')
+                var filter_001s = _.filter(items[0].balance.data, function(dt) {
+                    var end_date = moment(dt.end_date).format('YYYY-MM-DD')
+                    return dt.absence_code == '001' && (end_date == current_date || end_date > current_date)
+                })
+
+                var filter_002s = _.filter(items[0].balance.data, function(dt) {
+                    var end_date = moment(dt.end_date).format('YYYY-MM-DD')
+                    return dt.absence_code == '005' && dt.balance > 0
+                })
+
+                var filter_003s = _.filter(items[0].balance.details, function(dt) {
+                    var end_date = moment(dt.end_date).format('YYYY-MM-DD')
+                    return dt.absence_code == '001'
+                })
+
+                var filter_004s = _.filter(items[0].balance.details, function(dt) {
+                    var end_date = moment(dt.end_date).format('YYYY-MM-DD')
+                    return dt.absence_code == '005'
+                })
+
+                rendered_data = self.balance_template({
+                    '001_bs': sort(filter_003s),
+                    '002_bs': sort(filter_004s),
+                    '001_num': calculate(filter_001s),
+                    '002_num': calculate(filter_002s)
+                });
+
+            } else if (self.mode_view == '2') {
+                var all_items = _.flatten([items[0].leave_total.leaveOfabsences, items[0].leave_total.backleaveOfabsences], true)
+
+
+                var gb = _.groupBy(all_items, function(le) {
+                    return le.absence_code
+                })
+                var its = []
+                _.each(gb, function(ys, k) {
+                    var f_d = _.find(items[0].absences, function(ia) {
+                        return ia.absence_type_code == k
+                    })
+                    var num = 0;
+                    var times = [];
+                    _.each(ys, function(y) {
+                        num += y.hours;
+                        times.push({
+                            current_date: y.current_date,
+                            hours: y.hours,
+                            create_start_date: y.leave.create_start_date || y.leave.start_date, //生成请假明细开始日期
+                            create_end_date: y.leave.create_end_date || y.leave.start_date, //生成请假明细结束日期
+                        })
+                    })
+
+                    its.push({
+                        absence_name: f_d.absence_type_name,
+                        total_hours: num,
+                        details: times
+                    })
+
+                })
+                rendered_data = self.balance_total_template({
+                    leaveOfabsences: its
+                });
+
+            };
             // console.log(self.collection.toJSON())
             $("#leave_list-content").html(rendered_data);
             $("#leave_list-content").trigger('create');
@@ -72,9 +181,42 @@ define(["jquery", "underscore", "backbone", "handlebars", "async", "moment"], fu
             }).on('click', '#crate_leave', function(event) {
                 self.leaveOfAbsence.save().done(function(data) {
                     window.location.href = "/m#leave_form_t/" + data.ti._id;
-
+                })
+            }).on('click', '.open-left-panel', function(event) {
+                event.preventDefault();
+                $("#leave_list-left-panel").panel("open");
+            }).on('swiperight', function(event) { //向右滑动，打开左边的面板
+                event.preventDefault();
+                $("#leave_list-left-panel").panel("open");
+            }).on('click', '#btn-leave_list-refresh', function(event) {
+                event.preventDefault();
+                console.log('+++++++++++')
+                $.mobile.loading("show");
+                self.collection.fetch().done(function() {
+                    self.render();
+                    $.mobile.loading("hide");
+                    $("#leave_list-left-panel").panel("close");
                 })
 
+            }).on('change', 'input[type="radio"]', function(event) {
+                event.preventDefault();
+                // console.log('=======')
+                // console.log($(this).val())
+                self.mode_view = $(this).val();
+                self.render();
+                // var $this = $(this);
+                // var field = $this.data("field");
+                // var value = $this.val();
+                // if (field == 'date_period') { //需要重新获取数据
+                //     $.mobile.loading("show");
+                //     self.period_set = value
+                // } else if (field == 'date_qt_type') {
+                //     $.mobile.loading("show");
+                //     self.date_typeset = value
+                // }
+                // self.render();
+                // $.mobile.loading("hide");
+                $("#leave_list-left-panel").panel("close");
             })
         },
 
