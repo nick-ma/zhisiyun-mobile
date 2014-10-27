a49da45021894d575541b6fb7dd5acceea66914e
blithe <blithe@blithedeMacBook-Pro.local>
2014-10-27 15:39:52 +0800

    工作报告

--- views/WorkReportListView.js
+++ views/WorkReportListView.js
@@ -7,19 +7,21 @@ define(["jquery", "underscore", "backbone", "handlebars"],
 
             initialize: function() {
                 this.template = Handlebars.compile($("#tmp_wr_list_view").html());
+                this.template_sub = Handlebars.compile($("#tmp_wr_sub_list_view").html());
+                this.filter_mode = '0';
+                this.sub_items = [];
+                this.current_time = '';
                 this.bind_event();
             },
 
             // Renders all of the Contact models on the UI
             render: function() {
                 var self = this;
-
                 self.wr_list_back_url = localStorage.getItem('wr_list_back_url') || null;
                 localStorage.removeItem('wr_list_back_url'); //用完删掉 
-                if (self.wr_list_back_url) {//有才设，没有则保持不变
+                if (self.wr_list_back_url) { //有才设，没有则保持不变
                     $("#btn-wr_list-back").attr('href', self.wr_list_back_url);
                 }
-
                 var render_data = {
                     wrs: [],
                 }
@@ -30,15 +32,47 @@ define(["jquery", "underscore", "backbone", "handlebars"],
                             render_data.wrs.push(x.attributes)
                         }
                     })
-                } else { //未提交
+                    $("#wr_list-content").html(self.template(render_data));
+                } else if (self.filter_mode == '0') { //未提交
                     _.each(self.collection.models, function(x) {
                         if (!x.attributes.is_submit && moment(x.attributes.show_time) < moment().add(1, 'months')) {
                             render_data.wrs.push(x.attributes)
                         }
                     })
+                    $("#wr_list-content").html(self.template(render_data));
+                } else {
+                    var group = _.groupBy(self.sub_items, function(qt) {
+                        return moment(qt.show_time).format('YYYY-MM-DD')
+                    })
+                    var sorts = _.sortBy(_.keys(group), function(gp) {
+                        return gp
+                    })
+                    if (!self.current_time) {
+                        self.current_time = _.last(sorts);
+                    };
+                    render_data.wrs = group[self.current_time];
+
+                    $("#wr_list-content").html(self.template_sub(render_data));
                 }
-                $("#wr_list-content").html(self.template(render_data));
+
                 $("#wr_list-content").trigger('create');
+
+
+                if (group[self.current_time].length) {
+                    $('#sub_show_work').show()
+                }
+
+
+                var items = []
+                _.each(group, function(ys, k) {
+                    if (self.current_time == k) {
+                        items.push('<option value=' + k + ' selected>' + k + '</option>')
+                    } else {
+                        items.push('<option value=' + k + ' >' + k + '</option>')
+                    }
+                })
+                $("#sub_work_times").html(items.join(''))
+                $("#sub_work_times").prev().html(self.current_time)
 
                 return this;
 
@@ -49,6 +83,26 @@ define(["jquery", "underscore", "backbone", "handlebars"],
                     event.preventDefault();
                     self.filter_mode = $(this).val();
                     self.render();
+                }).on('click', '.btn-wr-change_state', function(event) {
+                    event.preventDefault();
+                    var $this = $(this);
+                    var state = $this.data('state');
+                    if (state == '0' || state == '1') {
+                        self.filter_mode = state
+                        self.render();
+                    } else {
+                        $.get('/admin/pm/work_report/get_sub_work_reports', function(data) {
+                            self.sub_items = data;
+                            self.filter_mode = state
+                            self.render();
+                        })
+
+                    }
+                }).on('change', '#sub_work_times', function(event) {
+                    event.preventDefault();
+                    self.current_time = $(this).val();
+                    self.render();
+
                 })
             }
 
