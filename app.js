826814c382305b60b554e06a93f2dee3716586e8
Nick Ma <honeyday.mj@gmail.com>
2014-11-25 23:46:15 +0800

    优化加载

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Sets the require.js configuration for your application.
 require.config({
+  waitSeconds: 1000,
   // add one sentence only for test the ivan branch.
   // 3rd party script alias names (Easier to type "jquery" than "libs/jquery-1.8.2.min")
   paths: {
--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ scp -pC ../../views/m.jade root@www.zhisiyun.com:/var/gisiclouds/www/gisiclouds/
 scp -rC ../../views/mobile/ root@www.zhisiyun.com:/var/gisiclouds/www/gisiclouds/views
 scp -pC _static/app.js root@www.zhisiyun.com:/var/gisiclouds/www/gisiclouds/client/m/_static
 scp -pC _static/main.css root@www.zhisiyun.com:/var/gisiclouds/www/gisiclouds/client/m/_static
-# scp -C _static/require.js root@www.zhisiyun.com:/var/gisiclouds/www/gisiclouds/client/m/_static
+scp -C _static/require.js root@www.zhisiyun.com:/var/gisiclouds/www/gisiclouds/client/m/_static
 
 # scp -rC _static/ root@www.zhisiyun.com:/var/gisiclouds/www/gisiclouds/client/m
