3ac54788bb13bf775581199f587abd52a7ece7eb
GISI Dev 02 <gisidev02@ubuntu.(none)>
2015-01-21 10:17:21 +0800

    通知公告

---
+++ collections/NotificationCollection.js
@@ -0,0 +1,16 @@
+// Includes file dependencies
+define(["jquery", "backbone", "models/NotificationModel"], function($, Backbone, NotificationModel) {
+
+    // Extends Backbone.Router
+    var Collection = Backbone.Collection.extend({
+        date_offset: 30,
+        // Sets the Collection model property to be a People Model
+        model: NotificationModel,
+        url: function() {
+            return '/wxapp/005/bb?date_offset=' + this.date_offset;
+        },
+    });
+
+    // Returns the Model class
+    return Collection;
+});
